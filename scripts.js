12eabc6913730e940aef68e0557f44cde7b01fb4
antonionetopxxp <159489586+antonionetopxxp@users.noreply.github.com>
2026-02-19 14:32:27 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,6 +6,29 @@ function mostrarForm() {
     mascara.style.visibility = "visible"
 }
 
+const openMenu = document.getElementById('openMenu');
+const closeMenu = document.getElementById('closeMenu');
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+
+// Função para abrir
+openMenu.addEventListener('click', () => {
+    sidebar.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden'; // Trava o scroll do fundo
+});
+
+// Função para fechar
+const closeFunc = () => {
+    sidebar.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto'; // Libera o scroll
+};
+
+closeMenu.addEventListener('click', closeFunc);
+overlay.addEventListener('click', closeFunc);
+
+
 function esconderForm() {
     formulario.style.left = "-300px"
     mascara.style.visibility = "hidden"
